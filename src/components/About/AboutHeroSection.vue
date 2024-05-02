7a6111548ce1c0e40c8588724a9a5f1763055e62
dagbuelawrence <dagbuel@gmail.com>
2024-05-02 01:56:09 +0100

    first push

---
+++ src/components/About/AboutHeroSection.vue
@@ -0,0 +1,266 @@
+<template>
+  <div class="alpha">
+
+<!--    <main>-->
+
+<!--      <div class="main-intro">-->
+<!--        <h1> Why choose <br> Income Trade <br> Asset </h1>-->
+<!--&lt;!&ndash;        <p> Maximize your trading success with our superior, <br> market conditions, offering you the optimal <br> advantage for financial excellence.</p>&ndash;&gt;-->
+<!--&lt;!&ndash;        <p class="btn">Register</p>&ndash;&gt;-->
+<!--      </div>-->
+
+<!--      <div class="main-quotes">-->
+<!--        <p>"We aim to deliver the ultimate online trading experience <br> by passionately innovating and continually enhancing <br> the way you invest in financial markets."</p>-->
+
+<!--        <p>"We aim is to deliver an unparalleled trading experience <br> by passionately innovating and enhancing how <br> you invest in financial markets online."</p>-->
+<!--      </div>-->
+
+<!--    </main>-->
+
+
+    <div class="section-alpha">
+
+      <div class="section-1">
+
+        <div class="content-1 right">
+          <p class="text-1">Withdrawals</p>
+          <p class="text-2">Instant withdrawals</p>
+          <p class="text-3">Get your deposits and withdrawals approved the moment you click the button.</p>
+        </div>
+
+        <div class="content-1 right">
+          <p class="text-1">Spreads</p>
+          <p class="text-2">Tight & stable spreads</p>
+          <p class="text-3">Trade confidently in times of volatility with low and reliable spreads.</p>
+        </div>
+
+      </div>
+
+      <div class="section-1">
+
+        <div class="content-1 left">
+          <p class="text-1">Execution speed</p>
+          <p class="text-2">Ultra-fast execution</p>
+          <p class="text-3">Execute your orders in milliseconds, no matter how big they are.</p>
+        </div>
+
+        <div class="content-1 left">
+          <p class="text-1">Swaps</p>
+          <p class="text-2">No overnight fees</p>
+          <p class="text-3">Hold your leveraged positions for as long as you like, swap-free. T&C apply.</p>
+        </div>
+
+      </div>
+
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutHeroSection"
+}
+</script>
+
+<style scoped>
+.alpha{
+  width: 100%;
+  height: 90vh;
+  background-color: #070e20;
+  background-image: url("@/assets/abouthero.jpeg");
+  background-size: 50vh, cover;
+  background-repeat: no-repeat;
+  background-position: bottom, center;
+  /*display: flex;*/
+  /*justify-content: center;*/
+  /*align-items: center;*/
+  /*align-content: center;*/
+  /*column-gap: 20px;*/
+  z-index: 100;
+  color: #FFFFFF;
+}
+.section-alpha{
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+}
+
+.section-1{
+  margin-top: 15%;
+}
+
+.content-1{
+  margin-bottom: 15%;
+  width: 70%;
+  text-align: center;
+}
+
+.left{
+  position: relative;
+  left: 25vh;
+}
+
+.right{
+  position: relative;
+  right: 4vh;
+}
+
+.text-1{
+  margin-bottom: 10px;
+  padding: 7px 15px;
+  border-radius: 20px;
+  background-color: #6c8595;
+  display: inline-block;
+}
+
+.text-2{
+  margin-bottom: 5px;
+  font-size: 18px;
+}
+
+
+
+/*h1{*/
+/*  font-size: 24px;*/
+/*  line-height: 32px;*/
+/*  color: #ffffff;*/
+/*  font-weight: 600;*/
+/*  !*text-transform: uppercase;*!*/
+/*}*/
+/*p{*/
+/*  font-size: 14px;*/
+/*  line-height: 18px;*/
+/*  color: #ffffff;*/
+/*}*/
+main{
+  width: 100%;
+  height: 90vh;
+  background-color: #070e20;
+  background-image: url("@/assets/abouthero.jpeg");
+  background-size: 50vh, cover;
+  background-repeat: no-repeat;
+  background-position: bottom, center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  /*column-gap: 20px;*/
+  z-index: 100;
+}
+
+main .main-intro h1 {
+  font-size: 53px;
+  line-height: 55px;
+  letter-spacing: 1.8px;
+  padding-bottom: 5px;
+}
+
+main  p {
+  font-size: 18px;
+  line-height: 30px;
+}
+
+.btn{
+  margin-top: 15px;
+  font-size: 18px;
+  display: block;
+  background-color: #ccaa16;
+  color: #FFFFFF;
+  padding: 5px 30px;
+  width: fit-content;
+  border-radius: 4px;
+}
+
+main .main-quotes p {
+  /*border-left: 4px solid #ffcf01;*/
+  margin: 40px;
+}
+
+main .main-quotes p:nth-child(2){
+  /*margin-left: 120px;*/
+}
+
+main .main-intro  {
+  position: relative;
+  right: 4vh;
+  padding-bottom: 8vh;
+}
+
+main .main-quotes  {
+  position: relative;
+  /*left: 18vh;*/
+}
+
+
+@keyframes showContent {
+  to{
+    transform: translateY(0);
+    filter: blur(0);
+    opacity: 1;
+  }
+}
+
+main .main-intro h1,
+main p{
+  transform: translateY(30px);
+  filter: blur(20px);
+  opacity: 0;
+  animation: showContent .4s .6s ease-in-out 1 forwards;
+}
+
+main .main-intro h1{
+  animation-delay: 0.8s;
+}
+
+main p{
+  animation-duration: 1.2s;
+}
+
+@media screen and (max-width: 980px) {
+
+}
+
+@media screen and (max-width: 760px) {
+  main .main-intro h1 {
+    font-size: 35px;
+    line-height: 35px;
+    letter-spacing: 1.5px;
+  }
+
+  main{
+    background-size: 45vh, cover;
+  }
+
+
+
+  main .main-quotes p:nth-child(2){
+    margin-left: unset;
+  }
+
+  main .main-intro  {
+    position: relative;
+    right: unset;
+    padding-bottom: unset;
+    left: 4vh;
+  }
+
+  main .main-quotes  {
+    position: relative;
+    left: unset;
+  }
+
+  main  p {
+    font-size: 17px;
+    line-height: 25px;
+  }
+
+
+}
+
+@media screen and (max-width: 500px) {
+
+}
+</style>
---
+++ src/components/Home/GetStarted.vue
@@ -0,0 +1,239 @@
+<template>
+
+  <div class="alpha">
+    <div class="timeline">
+      <p class="section-header-1">Get started</p>
+      <p class="section-header">Start trading with ease on our platform.</p>
+      <div class="timeline__event  animated fadeInUp delay-3s timeline__event--type1">
+        <div class="timeline__event__icon ">
+          <i class="fas fa-tarp"></i>
+          <div class="timeline__event__date">
+            Step 1
+          </div>
+        </div>
+        <div class="timeline__event__content ">
+          <div class="timeline__event__title">
+            Create Account
+          </div>
+          <div class="timeline__event__description">
+            <p>Create an account and deposit funds to get started.</p>
+          </div>
+        </div>
+      </div>
+      <div class="timeline__event animated fadeInUp delay-2s timeline__event--type2">
+        <div class="timeline__event__icon">
+          <i class="fas fa-project-diagram"></i>
+          <div class="timeline__event__date">
+            Step 2
+          </div>
+        </div>
+        <div class="timeline__event__content">
+          <div class="timeline__event__title">
+            Choose Currency
+          </div>
+          <div class="timeline__event__description">
+            <p>Choose the currency pairs you want to trade.</p>
+          </div>
+        </div>
+      </div>
+      <div class="timeline__event animated fadeInUp delay-1s timeline__event--type3">
+        <div class="timeline__event__icon">
+          <i class="fas fa-project-diagram"></i>
+          <div class="timeline__event__date">
+            Step 3
+          </div>
+        </div>
+        <div class="timeline__event__content">
+          <div class="timeline__event__title">
+            Analyze market
+          </div>
+          <div class="timeline__event__description">
+            <p>Analyze market trends and make informed trading decisions.</p>
+          </div>
+
+        </div>
+      </div>
+      <div class="timeline__event animated fadeInUp timeline__event--type1">
+        <div class="timeline__event__icon">
+          <i class="fas fa-graduation-cap"></i>
+          <div class="timeline__event__date">
+            Step 4
+          </div>
+        </div>
+        <div class="timeline__event__content">
+          <div class="timeline__event__title">
+            Execute trades
+          </div>
+          <div class="timeline__event__description">
+            <p>Execute trades and monitor your portfolio performance.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "GetStarted"
+}
+</script>
+
+<style scoped>
+.alpha{
+  background-image: url("@/assets/background.png");
+  /*background-color: #000;*/
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+  padding-bottom: 5%;
+  padding-top: 5%;
+}
+
+.section-header{
+  font-size: 17px;
+  color: #0f171c;
+  text-align: center;
+  font-weight: 300;
+  margin-left: 10%;
+  margin-right: 10%;
+  padding-bottom: 1%;
+}
+.section-header-1{
+  font-size: 30px;
+  color: #222222;
+  font-weight: bold;
+  text-align: center;
+  padding-bottom: 0.5%;
+  font-family: 'BR-Firma-Bold', sans-serif;
+}
+
+.timeline {
+  display: flex;
+  flex-direction: column;
+  width: 50vw;
+  margin: auto;
+}
+
+.timeline__event {
+  background: #fff;
+  margin-bottom: 20px;
+  position: relative;
+  display: flex;
+  margin: 20px 0;
+  border-radius: 8px;
+  box-shadow: 0 30px 60px -12px rgba(50, 50, 93, 0.25), 0 18px 36px -18px rgba(0, 0, 0, 0.3), 0 -12px 36px -8px rgba(0, 0, 0, 0.025);
+}
+.timeline__event__title {
+  font-size: 1.2rem;
+  line-height: 1.4;
+  text-transform: uppercase;
+  font-weight: 600;
+  color: #9251ac;
+  letter-spacing: 1.5px;
+  font-family: 'BR-Firma-Bold', sans-serif;
+}
+.timeline__event__content {
+  padding: 20px;
+}
+.timeline__event__date {
+  color: #ffffff;
+  font-size: 1.5rem;
+  font-weight: 600;
+  white-space: nowrap;
+}
+.timeline__event__icon {
+  border-radius: 8px 0 0 8px;
+  background: #9251ac;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-basis: 40%;
+  font-size: 2rem;
+  color: #9251ac;
+  padding: 20px;
+}
+.timeline__event__icon i {
+  position: absolute;
+  top: 50%;
+  left: -70px;
+  font-size: 2rem;
+  transform: translateY(-50%);
+}
+.timeline__event__description {
+  flex-basis: 60%;
+}
+.timeline__event:after {
+  content: "";
+  width: 2px;
+  height: 100%;
+  background: #9251ac;
+  position: absolute;
+  top: 52%;
+  left: -3.5rem;
+  z-index: -1;
+}
+.timeline__event:before {
+  content: "";
+  width: 5rem;
+  height: 5rem;
+  position: absolute;
+  background: #f6a4ec;
+  border-radius: 100%;
+  left: -6rem;
+  top: 50%;
+  transform: translateY(-50%);
+  border: 2px solid #9251ac;
+}
+.timeline__event--type2:before {
+  background: #87bbfe;
+  border-color: #555ac0;
+}
+.timeline__event--type2:after {
+  background: #555ac0;
+}
+.timeline__event--type2 .timeline__event__date {
+  color: #ffffff;
+}
+.timeline__event--type2 .timeline__event__icon {
+  background: #555ac0;
+  color: #555ac0;
+}
+.timeline__event--type2 .timeline__event__title {
+  color: #555ac0;
+}
+.timeline__event--type3:before {
+  background: #aff1b6;
+  border-color: #24b47e;
+}
+.timeline__event--type3:after {
+  background: #24b47e;
+}
+.timeline__event--type3 .timeline__event__date {
+  color: #ffffff;
+}
+.timeline__event--type3 .timeline__event__icon {
+  background: #24b47e;
+  color: #24b47e;
+}
+.timeline__event--type3 .timeline__event__title {
+  color: #24b47e;
+}
+.timeline__event:last-child:after {
+  content: none;
+}
+
+@media (max-width: 786px) {
+  .timeline__event {
+    flex-direction: column;
+  }
+
+  .timeline__event__icon {
+    border-radius: 4px 4px 0 0;
+  }
+}
+</style>
